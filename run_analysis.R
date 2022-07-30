1648f4735e99d1b45906fe3ba19d9292ccf591e2
dipagh <105732302+dipagh@users.noreply.github.com>
2022-07-30 13:39:41 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -55,7 +55,7 @@ names(df_tidy)<-gsub("gravity", "Gravity", names(df_tidy))
 #5. From the data set in step 4, creates a second, independent tidy data set 
 #with the average of each variable for each activity and each subject.
 
-tidy_final<-aggregate(. ~subject + id, df_tidy, mean)
+tidy_final<-aggregate(. ~subject + activity, df_tidy, mean)
 
 str(tidy_final)
 
